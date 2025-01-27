b2e8ff481f28d3985f6df7d0b98d3cad67c9a7e1
ANNSUN-Y <rokes673@gmail.com>
2025-01-27 15:06:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,9 +7,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=p44GnDZS#raqFDGlBUYRoMx7joJVyHkPVHoZ3I8dwPK708BhdKlM",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
-MONGODB: process.env.MONGODB || "Put Your Mongode Url 🖇️",
+MONGODB: process.env.MONGODB || "6797526abb361d6c1ff11618",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
